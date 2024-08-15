ad99423930147411f9d7711089ce27cab1d0a4ec
MariiaLumpsikovna <mboorakova@gmail.com>
2024-08-15 22:27:28 +0300

    Add files via upload

---
+++ peremennyie.py
@@ -0,0 +1,5 @@
+homeworks = 12
+hours = 1.5
+name_of_course = 'Python'
+time = hours/homeworks
+print('Курс:', name_of_course, ', всего задач:', homeworks, ', затрачено часов:', hours, ', среднее время выполнения', time, '.')
